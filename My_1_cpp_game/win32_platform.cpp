dfc8dd604fff1e82989c6d5adc5382ea3479dbd3
Luke_THEVARD <luke.thevard@gmail.com>
2023-12-15 13:12:06 +0100

    Ajuster la taille de la fenêtre + choix de la couleur de fond et ajout du .gitignore

--- My_1_cpp_game/win32_platform.cpp
+++ My_1_cpp_game/win32_platform.cpp
@@ -2,6 +2,11 @@
 
 bool running = true; 
 
+void* buffer_memory;
+int buffer_width;
+int buffer_height;
+BITMAPINFO buffer_bitmap_info;
+
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0; 
 
@@ -10,6 +15,27 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 		case WM_DESTROY: {
 			running = false;
 		} break; 
+
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			buffer_width = rect.right - rect.left;
+			buffer_height = rect.bottom - rect.top;
+
+			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int); 
+
+			if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
+			buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
+			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+			buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
+			buffer_bitmap_info.bmiHeader.biPlanes = 1;
+			buffer_bitmap_info.bmiHeader.biBitCount = 32;
+			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		}break; 
+		
 		default: {
 			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 		}
@@ -29,7 +55,8 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	
 	//Create window
 	HWND window = CreateWindowA(window_class.lpszClassName, "mon premier jeu", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
-
+	HDC hdc = GetDC(window);
+	
 	while (running){
 		//input
 		MSG message; 
@@ -40,8 +67,15 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		}
 
 		//simulate
+		unsigned int* pixel = (unsigned int*)buffer_memory; 
+		for (int y = 0; y < buffer_height; y++) {
+			for (int x = 0; x < buffer_width; x++) {
+				*pixel++ = 0x424242;
+			}
+		}
 
 		//render
+		StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 
 	}
 }
